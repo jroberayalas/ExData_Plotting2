d76422975c34b12b4d3a47e332826e33064abc7d
ichbinjras <ichbinjras@gmail.com>
2014-10-16 22:09:59 +0100

    Add plot1
    Add code and figure for plot 1.

---
+++ plot1.R
@@ -0,0 +1,20 @@
+# Read data
+NEI <- readRDS("Data/summarySCC_PM25.rds")
+SCC <- readRDS("Data/Source_Classification_Code.rds")
+
+# Transform year variable to factor
+NEI <- transform(NEI, year = factor(year))
+
+# Use dplyr library to group by year and summarise the sum of Emissions
+library(dplyr)
+NEI <- tbl_df(NEI)
+sums.summary <- NEI %>%
+    group_by(year) %>%
+    summarize(sums = sum(Emissions))
+
+# Plotting
+png(file = "ExData_Plotting2/plot1.png", bg = "transparent")
+with(sums.summary, plot(year, sums, 
+                        xlab = "Year",
+                        ylab = expression("Total emissions from PM"[2.5])))
+dev.off()
